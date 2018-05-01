ba8918c31729959782b15f2488ede349e6baac08
dariush fathi <dariush.fathe@gmail.com>
2018-05-01 09:16:11 +0430

    updates : added crashlytics and Analytics

--- app/src/main/java/ir/paad/ztest/adapters/GroupItemAdpater.kt
+++ app/src/main/java/ir/paad/ztest/adapters/GroupItemAdpater.kt
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import ir.paad.ztest.R
 
 class GroupItemAdapter(ctx:Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -15,7 +17,7 @@ class GroupItemAdapter(ctx:Context) : RecyclerView.Adapter<RecyclerView.ViewHold
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+       // Glide.with(context).load("").apply(RequestOptions().centerCrop()).into(holder.imageView)
     }
 
     override fun getItemCount(): Int {
